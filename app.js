f371a4e3eeb880f262cd8e07a7ccfcd973b95fda
AndrewHemry <Andrewhemry@gmail.com>
2022-07-23 17:20:45 -0500

    Updating files with JS and CSS for alignment

--- app.js
+++ app.js
@@ -8,18 +8,3 @@ if (btnClear) {
         inputMessage.value = '';
     });
 };
-
-
-
-
-
-
-
-
-
-// btnClear.addEventListener('click', () => {
-//     const inputName = document.getElementById('.nameField');
-//     const inputMessage = document.getElementById('.messageField');
-//     inputName.value = '';
-//     inputMessage.value = ''
-// });
